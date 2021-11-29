ca15888d55cf26c4949422df057a1095ab7146ef
Mohd Feroz Baig <feroz.baig96@gmail.com>
2021-11-29 20:42:08 +0530

    added lambda

--- src/main/java/com/myorg/CdkWorkshopStack.java
+++ src/main/java/com/myorg/CdkWorkshopStack.java
@@ -4,6 +4,9 @@ import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Duration;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
 import software.amazon.awscdk.services.sns.Topic;
 import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
 import software.amazon.awscdk.services.sqs.Queue;
@@ -16,6 +19,11 @@ public class CdkWorkshopStack extends Stack {
     public CdkWorkshopStack(final Construct parent, final String id, final StackProps props) {
         super(parent, id, props);
 
-        // Nothing here!
+        // lambda
+        final Function hello = Function.Builder.create(this, "HelloHandler")
+            .runtime(Runtime.NODEJS_14_X)
+            .code(Code.fromAsset("src/lambda"))
+            .handler("hello.handler")
+            .build();
     }
 }
