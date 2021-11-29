5d16c22e6673be5806dcec5fd505bb7f770400db
Mohd Feroz Baig <feroz.baig96@gmail.com>
2021-11-29 22:50:10 +0530

    added resources to HitCounter construct

--- src/main/java/com/myorg/HitCounter.java
+++ src/main/java/com/myorg/HitCounter.java
@@ -1,12 +1,49 @@
 package com.myorg;
 
+import software.amazon.awscdk.services.dynamodb.Attribute;
+import software.amazon.awscdk.services.dynamodb.AttributeType;
+import software.amazon.awscdk.services.dynamodb.Table;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
 import software.constructs.Construct;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HitCounter extends Construct {
+
+	private final Function handler;
+	private final Table table;
 
 	public HitCounter(final Construct scope, final String id, final HitCounterProps props) {
 		super(scope, id);
 
-		// TODO
+		this.table = Table.Builder.create(this, "Hits")
+			.partitionKey(Attribute.builder()
+				.name("path")
+				.type(AttributeType.STRING)
+				.build())
+			.build();
+
+		final Map<String, String> environment = new HashMap<>();
+		environment.put("DOWNSTREAM_FUNCTION_NAME", props.getDownstream().getFunctionName());
+		environment.put("HITS_TABLE_NAME", this.table.getTableName());
+
+		this.handler = Function.Builder.create(this, "HitCounterHandler")
+			.runtime(Runtime.NODEJS_14_X)
+			.handler("hitcounter.handler")
+			.functionName("hitcounter")
+			.code(Code.fromAsset("src/lambda"))
+			.environment(environment)
+			.build();
+	}
+
+	public Function getHandler() {
+		return handler;
+	}
+
+	public Table getTable() {
+		return table;
 	}
 }
