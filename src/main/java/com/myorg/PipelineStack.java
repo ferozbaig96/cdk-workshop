e1a86a83418c3b018527c62a53fb8fd2f78728ed
Mohd Feroz Baig <feroz.baig96@gmail.com>
2021-11-30 12:26:59 +0530

    added codecommit

--- src/main/java/com/myorg/PipelineStack.java
+++ src/main/java/com/myorg/PipelineStack.java
@@ -3,6 +3,7 @@ package com.myorg;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.codecommit.Repository;
 
 public class PipelineStack extends Stack {
 	public PipelineStack(final Construct parent, final String id) {
@@ -12,6 +13,9 @@ public class PipelineStack extends Stack {
 	public PipelineStack(final Construct parent, final String id, final StackProps props) {
 		super(parent, id, props);
 
-		// Pipeline code goes here
+		// CodeCommit repo 'WorkshopRepo'
+		final Repository repo = Repository.Builder.create(this, "WorkshopRepo")
+			.repositoryName("WorkshopRepo")
+			.build();
 	}
 }
