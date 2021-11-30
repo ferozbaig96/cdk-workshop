bc200f46a3188fef1155f25eea7d6b79874d3eb8
Mohd Feroz Baig <feroz.baig96@gmail.com>
2021-11-30 12:40:35 +0530

    added codepipieline

--- src/main/java/com/myorg/PipelineStack.java
+++ src/main/java/com/myorg/PipelineStack.java
@@ -3,7 +3,12 @@ package com.myorg;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.pipelines.CodeBuildStep;
+import software.amazon.awscdk.pipelines.CodePipeline;
+import software.amazon.awscdk.pipelines.CodePipelineSource;
 import software.amazon.awscdk.services.codecommit.Repository;
+
+import java.util.Arrays;
 
 public class PipelineStack extends Stack {
 	public PipelineStack(final Construct parent, final String id) {
@@ -16,6 +21,21 @@ public class PipelineStack extends Stack {
 		// CodeCommit repo 'WorkshopRepo'
 		final Repository repo = Repository.Builder.create(this, "WorkshopRepo")
 			.repositoryName("WorkshopRepo")
+			.build();
+
+		// CodePipeline
+		final CodePipeline pipeline = CodePipeline.Builder.create(this, "Pipeline")
+			.pipelineName("WorkshopPipeline")
+			.synth(CodeBuildStep.Builder.create("SynthStep")
+				.input(CodePipelineSource.codeCommit(repo, "master"))
+				.installCommands(Arrays.asList(
+					"npm install -g aws-cdk"  // Commands to run before build
+				))
+				.commands(Arrays.asList(
+					"mvn package",
+					"npx cdk synth"
+				))
+				.build())
 			.build();
 	}
 }
