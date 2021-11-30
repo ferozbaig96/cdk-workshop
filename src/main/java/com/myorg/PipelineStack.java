2cace417a76a6be0ac605f156b771790ab331226
Mohd Feroz Baig <feroz.baig96@gmail.com>
2021-11-30 13:17:43 +0530

    added multi actions in single stage for TEST

--- src/main/java/com/myorg/PipelineStack.java
+++ src/main/java/com/myorg/PipelineStack.java
@@ -1,14 +1,18 @@
 package com.myorg;
 
+import software.amazon.awscdk.core.CfnOutput;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
 import software.amazon.awscdk.pipelines.CodeBuildStep;
 import software.amazon.awscdk.pipelines.CodePipeline;
 import software.amazon.awscdk.pipelines.CodePipelineSource;
+import software.amazon.awscdk.pipelines.StageDeployment;
 import software.amazon.awscdk.services.codecommit.Repository;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PipelineStack extends Stack {
 	public PipelineStack(final Construct parent, final String id) {
@@ -40,6 +44,29 @@ public class PipelineStack extends Stack {
 
 		// CodePipeline Stage to deploy application
 		final WorkshopPipelineStage deploy = new WorkshopPipelineStage(this, "Deploy");
-		pipeline.addStage(deploy);
+		StageDeployment stageDeployment = pipeline.addStage(deploy);
+
+		// Tests phase
+		Map<String, CfnOutput> hashMap1 = new HashMap<>();
+		hashMap1.put("ENDPOINT_URL", deploy.hcViewerUrl);
+		Map<String, CfnOutput> hashMap2 = new HashMap<>();
+		hashMap1.put("ENDPOINT_URL", deploy.hcEndpoint);
+
+		stageDeployment.addPost(
+			CodeBuildStep.Builder.create("TestViewerEndpoint")
+				.projectName("TestViewerEndpoint")
+				.commands(Arrays.asList("curl -Ssf $ENDPOINT_URL"))
+				.envFromCfnOutputs(hashMap1)
+				.build(),
+			CodeBuildStep.Builder.create("TestAPIGatewayEndpoint")
+				.projectName("TestAPIGatewayEndpoint")
+				.envFromCfnOutputs(hashMap2)
+				.commands(Arrays.asList(
+					"curl -Ssf $ENDPOINT_URL",
+					"curl -Ssf $ENDPOINT_URL/hello",
+					"curl -Ssf $ENDPOINT_URL/test"
+				))
+				.build()
+		);
 	}
 }
--- src/main/java/com/myorg/WorkshopPipelineStage.java
+++ src/main/java/com/myorg/WorkshopPipelineStage.java
@@ -1,14 +1,21 @@
 package com.myorg;
 
+import software.amazon.awscdk.core.CfnOutput;
 import software.amazon.awscdk.core.Stage;
 import software.amazon.awscdk.core.StageProps;
 import software.constructs.Construct;
 
 public class WorkshopPipelineStage extends Stage {
+
+	public final CfnOutput hcViewerUrl;
+	public final CfnOutput hcEndpoint;
+
 	public WorkshopPipelineStage(final Construct scope, final String id, final StageProps props) {
 		super(scope, id, props);
 
-		new CdkWorkshopStack(this, "WebService");
+		final CdkWorkshopStack service = new CdkWorkshopStack(this, "WebService");
+		hcViewerUrl = service.hcViewerUrl;
+		hcEndpoint = service.hcEndpoint;
 	}
 
 	public WorkshopPipelineStage(final Construct scope, final String id) {
